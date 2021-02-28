ab0e8254c1db2799120eb239a8c35a7025c70505
Kaugumme <Kaugumme@byom.de>
2021-02-28 18:39:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -121,3 +121,68 @@ setInterval(_ => {
 setInterval(_ => {
     document.querySelector('textarea[minlength="3"]') && [document.querySelector('textarea[minlength="3"]').setAttribute("minlength", 1)]
 }, 5999);
+
+
+
+
+
+
+
+
+
+
+
+/////////////////pr0no
+{
+    const //script vars
+        root = 'https://pr0gramm.com',
+        Top = "top",
+        New = "new",
+        cleanNew = `${root}/${New}/!%20-auto%20%26%20-genau%20mein%20Gemüse`,
+        cleanTop = `${root}/${Top}/!%20-auto%20%26%20-genau%20mein%20Gemüse`;
+    //custom filter method
+    const filter = arg => {
+        //initial filter mode
+        if (arg.mode === 1) {
+            //coming from new
+            if (location.href !== cleanNew && (location.href === `${root}/${New}`)) {
+                location.href = cleanNew;
+            }
+            //coming from initial (root) or top
+            if (location.href !== cleanTop && (
+                    location.href === `${root}/` || location.href === `${root}/${Top}`)) {
+                location.href = cleanTop;
+
+            }
+        }
+        //push filter mode
+        if (arg.mode === 2) {
+            let go = false;
+            switch (arg.url) {
+                case `/${New}`: //push to clean top
+                    location.href = cleanNew;
+                    break;
+                case `/${Top}`: //push to clean new
+                    location.href = cleanTop;
+                    break;
+            }
+            return go;
+        }
+    }
+
+    //filter initial call
+    filter({
+        mode: 1
+    }); { //catch push crap
+        var realPushState = history.pushState;
+        history.pushState = function() {
+
+            if (filter({ //ignore unfiltered pushies
+                    mode: 2,
+                    url: arguments[2]
+                })) return void 0;
+            //dun touch ( apply original push url)
+            return realPushState.apply(history, arguments);
+        };
+    }
+}
